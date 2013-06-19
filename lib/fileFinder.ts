77fbe36ed26722ebbe78e1f56e5a45c149c95d72
Anton <aulyanov@spotware.com>
2013-06-19 15:22:15 +0300

    clear and init

---
+++ lib/fileFinder.ts
@@ -0,0 +1,3 @@
+///<reference path='../defs/node/node.d.ts'/>
+
+var fs = require('fs');
---
+++ tests/testFileFinder.ts
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ant
+ * Date: 6/19/13
+ * Time: 2:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
